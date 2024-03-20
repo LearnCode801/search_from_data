0e2ac8638315590d4e37995f2eef29e434d110de
Muhammad Talha <107929962+LearnCode801@users.noreply.github.com>
2024-03-21 03:15:17 +0500

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -16,15 +16,20 @@ st.markdown(hide_st_style, unsafe_allow_html=True)
 with open('tfidf_vectorizer.pkl', 'rb') as file:
     vectorizer = pickle.load(file)
 
-def get_top_related_articles(user_input, df, vectorizer, tfidf_matrix, top_n):
-    user_input_tfidf = vectorizer.transform([user_input])
-    cosine_similarities = cosine_similarity(user_input_tfidf, tfidf_matrix).flatten()
-    top_indices = cosine_similarities.argsort()[-top_n:][::-1]
-    top_related_articles = df.iloc[top_indices]
+def get_top_related_articles(user_input, df, vectorizer, tfidf_matrix, top_n):
+
+    user_input_tfidf = vectorizer.transform([user_input])
+
+    cosine_similarities = cosine_similarity(user_input_tfidf, tfidf_matrix).flatten()
+
+    top_indices = cosine_similarities.argsort()[-top_n:][::-1]
+
+    top_related_articles = df.iloc[top_indices]
+
     return top_related_articles
 
 def main():
-    st.write('### Article Search Based on the "Title Similarity Search"')
+    st.write('## Article Search Based on the "Title Similarity Search"')
     st.write('### Whole Scrape Data') 
     df = pd.read_csv("medium.csv",encoding="latin1")
     df=df[['Title','Content','Headings','Author URL','Read Time','Date','Image URL']]
